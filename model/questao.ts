9cac685d5a94251e58ae5652ecc753d53f032bf1
augusto-mattos <guto.s.mattos@gmail.com>
2023-06-12 11:38:52 +0200

    class resposta

--- model/questao.ts
+++ model/questao.ts
@@ -1,11 +1,12 @@
+import RespostaModel from "./resposta"
+
 export default class QuestaoModel {
     #id: number
     #enunciado: string
-    #respostas: any[]
+    #respostas: RespostaModel[]
     #acertou: boolean
-    /// #respondida: boolean
 
-    constructor(id: number, enunciado: string, respostas: any[], acertou = false) {
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
         this.#id = id 
         this.#enunciado = enunciado
         this.#respostas = respostas
@@ -28,7 +29,10 @@ export default class QuestaoModel {
         return this.#acertou
     }
 
-    // get respondida() {
-        // FIXME : implementar esse método
-    // }
+    get respondida() {
+        for(let resposta of this.#respostas) {
+            if(resposta.revelada) return true 
+        }
+        return false
+    }
 }
